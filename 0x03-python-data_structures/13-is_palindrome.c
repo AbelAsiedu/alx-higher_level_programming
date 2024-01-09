a60a41d2820c72c14ffa4e3056e221df7ff4f04a
AbelAsiedu <141195146+AbelAsiedu@users.noreply.github.com>
2024-01-09 00:25:46 +0000

    Update 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,54 +1,53 @@
-#include"lists.h"
-#include<stdlib.h>
-#include<stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * *add_nodeint-addsanewnodeatthebeginningofalistint_tlist
- * *@head:headoflistint_t
- * *@n:inttoaddinlistint_tlist
- * *Return:addressofthenewelement,orNULLifitfailed
- * */
-listint_t*add_nodeint(listint_t**head,constintn)
+*add_nodeint - adds a new node at the beginning of a listint_t list
+*@head: head of listint_t
+*@n: int to add in listint_t list
+*Return: address of the new element, or NULL if it failed
+*/
+listint_t *add_nodeint(listint_t **head, const int n)
 {
-		listint_t*new;
+	listint_t *new;
 
-			new=malloc(sizeof(listint_t));
-				if(new==NULL)
-							return(NULL);
-					new->n=n;
-						new->next=*head;
-							*head=new;
-								return(new);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
 }
 /**
- * *is_palindrome-identifyifasynglelinkedlistispalindrome
- * *@head:headoflistint_t
- * *Return:1ifitispalindromeelse0
- * */
-intis_palindrome(listint_t**head)
+*is_palindrome - identify if a syngle linked list is palindrome
+*@head: head of listint_t
+*Return: 1 if it is palindrome else 0
+*/
+int is_palindrome(listint_t **head)
 {
-		listint_t*head2=*head;
-			listint_t*aux=NULL,*aux2=NULL;
+	listint_t *head2 = *head;
+	listint_t *aux = NULL, *aux2 = NULL;
 
-				if(*head==NULL||head2->next==NULL)
-							return(1);
-					while(head2!=NULL)
-							{
-										add_nodeint(&aux,head2->n);
-												head2=head2->next;
-													}
-						aux2=aux;
-							while(*head!=NULL)
-									{
-												if((*head)->n!=aux2->n)
-															{
-																			free_listint(aux);
-																						return(0);
-																								}
-														*head=(*head)->next;
-																aux2=aux2->next;
-																	}
-								free_listint(aux);
-									return(1);
+	if (*head == NULL || head2->next == NULL)
+		return (1);
+	while (head2 != NULL)
+	{
+		add_nodeint(&aux, head2->n);
+		head2 = head2->next;
+	}
+	aux2 = aux;
+	while (*head != NULL)
+	{
+		if ((*head)->n != aux2->n)
+		{
+			free_listint(aux);
+			return (0);
+		}
+		*head = (*head)->next;
+		aux2 = aux2->next;
+	}
+	free_listint(aux);
+	return (1);
 }
-
