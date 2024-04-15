8fc1696adaf351656e3031d8037a212304b6ac35
AbelAsiedu <141195146+AbelAsiedu@users.noreply.github.com>
2024-04-15 02:32:59 +0000

    Create relationship_city.py

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class City(Base):
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
+    
+    state = relationship("State", back_populates="cities")
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    
+    cities = relationship("City", cascade="all, delete", back_populates="state")
